82f9fef67a91808c574ef1b0b01c9349ef362cab
Tushar Pokle <tushar.pokle@gmail.com>
2014-03-26 00:12:40 +1100

    Wow! Such module

--- a-plugin/project.clj
+++ a-plugin/project.clj
@@ -1,6 +1,8 @@
-(defproject a-plugin "0.1.0-SNAPSHOT"
+(defproject a-plugin "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[me.raynes/conch "0.6.0"]
+  				 [commons-io/commons-io "2.4"]]
   :eval-in-leiningen true)
--- a-plugin/src/leiningen/a_plugin.clj
+++ a-plugin/src/leiningen/a_plugin.clj
@@ -1,6 +1,29 @@
-(ns leiningen.a-plugin)
+(ns leiningen.a-plugin
+  (:use [clojure.java.io]
+        [leiningen.clean :only (clean)]
+        [leiningen.uberjar :only (uberjar)]
+        [clojure.pprint :only (pprint)])
+	(:import java.io.File org.apache.commons.io.FileUtils))
+
+(defn make-directories-in [base & children]
+		(doseq [child children] 
+      (.mkdirs (file base child))))
+		
+(defn dist [project]
+  "Creates the distribution dir - target/dist - that will be packaged up"
+  (let [uberjar-file (file (uberjar project))
+        dist-dir (file (:target-path project) "dist")]
+    (make-directories-in dist-dir "bin" "lib" "conf" "logs")
+    (copy uberjar-file (file dist-dir "lib" "application.jar"))))
+
+(defn package [project]
+  "Packages up the target/dist dir")
 
 (defn a-plugin
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+
+  #_(pprint project)
+  (clean project)
+  (dist project)
+  (package project))
